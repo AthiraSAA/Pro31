2c0de07bae8ab0f5e83ead00c65adcd9d572c9c6
USER <athirasedu@gmail.com>
2024-01-12 16:48:19 +0530

    gfhfy

--- lib/Thirdpage.dart
+++ lib/Thirdpage.dart
@@ -1,11 +1,14 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'a.dart';
+
 class Thirdpage extends StatefulWidget {
-  const Thirdpage({super.key});
+  const Thirdpage(User user, {super.key});
 
   @override
   State<Thirdpage> createState() => _ThirdpageState();
@@ -17,12 +20,15 @@ class _ThirdpageState extends State<Thirdpage> {
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPoFAvFQ7hGfhF3NzMNc_jgNW5fmBHc2EKtPnqsweB-eikE_UF6igTkY2LzsoQjEtWOew&usqp=CAU",
     "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0a78ef47-01c1-43e2-a389-fc73690b9100/dfpeh37-70231edc-1ee7-4b28-a666-de1c94925e05.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBhNzhlZjQ3LTAxYzEtNDNlMi1hMzg5LWZjNzM2OTBiOTEwMFwvZGZwZWgzNy03MDIzMWVkYy0xZWU3LTRiMjgtYTY2Ni1kZTFjOTQ5MjVlMDUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.wIxBn_-oqEFKT7WuUsYjmHDCotOxaCGXDfP39W9z56c",
   ];
-  final List<String> items = [
-    'Laptop',
-    'Mobile',
-    'Headphone',
-    'Consoles',
+  List<Map> categories = [
+    {'name': 'Laptop', 'iconPath': 'assets/images/laptop.png'},
+    {'name': 'Mobile', 'iconPath': 'assets/images/mobile11.png'},
+    {'name': 'Headphone', 'iconPath': 'assets/images/heahphone.png'},
+    {'name': 'Console', 'iconPath': 'assets/images/consoles.png'},
+    {'name': 'Tablets', 'iconPath': 'assets/images/tab.png'},
   ];
+
+  get backgroundImage => null;
 
   @override
   Widget build(BuildContext context) {
@@ -31,91 +37,174 @@ class _ThirdpageState extends State<Thirdpage> {
         title: SvgPicture.asset('assets/images/appp1.svg', fit: BoxFit.cover),
         actions: [
           IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
-          CircleAvatar(),
+          CircleAvatar(
+          backgroundImage: AssetImage('assets/images/1.png'),
+            backgroundColor: Colors.transparent,
+          ),
         ],
       ),
-      body: Column(children: [
-        Column(
-          children: [
-            TextField(
-              onChanged: (value) {
-                // Here you can implement your search logic
-                print('Searching for: $value');
-              },
-              decoration: InputDecoration(
-                labelText: 'Search Product',
-                hintText: 'Enter product name',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Column(
+            children: [
+              TextField(
+                onChanged: (value) {
+                  // Here you can implement your search logic
+                  print('Searching for: $value');
+                },
+                decoration: InputDecoration(
+                  labelText: 'Search Product',
+                  hintText: 'Enter product name',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 200,
-              child: CarouselSlider(
-                options: CarouselOptions(
-                  height: 400.0,
-                  aspectRatio: 16 / 9,
-                  viewportFraction: 0.8,
-                  initialPage: 0,
-                  enableInfiniteScroll: true,
-                  reverse: false,
-                  autoPlay: true,
-                  autoPlayInterval: Duration(seconds: 3),
-                  autoPlayAnimationDuration: Duration(milliseconds: 800),
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                  enlargeCenterPage: true,
-                  enlargeFactor: 0.3,
-                  scrollDirection: Axis.horizontal,
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 200,
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: 400.0,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.8,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 3),
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.3,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                  items: image.map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: BoxDecoration(color: Colors.amber),
+                            child: Image.network(
+                              "${i}",
+                              fit: BoxFit.fill,
+                            ));
+                      },
+                    );
+                  }).toList(),
                 ),
-                items: image.map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: EdgeInsets.symmetric(horizontal: 5.0),
-                          decoration: BoxDecoration(color: Colors.amber),
-                          child: Image.network(
-                            "${i}",
-                            fit: BoxFit.fill,
-                          ));
-                    },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(children: [
+                Text(
+                  'Categories',
+                  style: GoogleFonts.poppins(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ]),
+            ],
+          ),
+          Container(
+            height: 150,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          margin: EdgeInsets.only(left: 20),
+                          // decoration: BoxDecoration(
+                          //     color: Colors.grey,
+                          //     borderRadius: BorderRadius.circular(10)),
+                          child: Image.asset(
+                            categories[index]['iconPath'],
+                            height: 100,
+                            width: 100,
+                          ),
+                        ),
+                        // SizedBox(
+                        //   height: 10,
+                        // ),
+                        Text(
+                          categories[index]['name'],
+                          style: TextStyle(color: Colors.black),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
                   );
-                }).toList(),
+                }),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Text("Top Products",
+                  style: GoogleFonts.poppins(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w700,
+                  )),
+            ],
+          ),
+          Container(
+            height: 504,
+            width: 328,
+            child: GridView(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 16/9,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(children: [
-              Text(
-                'Categories',
-                style: GoogleFonts.poppins(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w700,
+              children: [
+                Container(
+                  height: 156,
+                  width: 135,
+                  child: Image.asset("assets/images/1.png"),
                 ),
-              ),
-            ]),
-          ],
-        ),
+                Container(
+                  height: 156,
+                  width: 135,
+                  child: Image.asset("assets/images/2.png"),
+                ),
+                Container(
+                  height: 156,
+                  width: 135,
+                  child: Image.asset("assets/images/3.png"),
+                ),
+                Container(
+                  height: 156,
+                  width: 135,
+                  child: Image.asset("assets/images/4.png"),
+                ),
+              ],
+            ),
+          ),
 
-        ListTile(
-          leading: Icon(Icons.inbox),
-          title: Text("Primary"),
-        ),
-        ListTile(
-          leading: Icon(Icons.inbox),
-          title: Text("Primary"),
-        ),
-        ListTile(
-          leading: Icon(Icons.inbox),
-          title: Text("Primary"),
-        ),
-      ]),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => A()),
+                );
+              },
+              child: Text(
+                "View more",
+              )),
+        ]),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:pro31/Thirdpage.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 
-import 'Thirdpage.dart';
-
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyBr12E7ktX8NwWlKb0P11qlhAfI46TC22c",
+      appId: "1:1026123339623:android:b757cd47f785d1594881c2",
+      messagingSenderId: "1026123339623",
+      projectId: "pro3-f9139",
+    ),
+  );
   runApp(const MyApp());
-}
 
+
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -39,78 +49,352 @@ class _MyHomePageState extends State<MyHomePage> {
             ()=>Navigator.pushReplacement(context,
             MaterialPageRoute(builder:
                 (context) =>
-                SecondScreen()
+                    LoginPage()
             )
         )
     );
   }
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        height: 104,
-        width: 104,
-        child: SvgPicture.asset("assets/images/Logo112.svg"),
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 104,
+          width: 104,
+          child: SvgPicture.asset("assets/images/Logo112.svg"),
+        ),
       ),
     );
   }
 }
 
-class SecondScreen extends StatefulWidget {
-  const SecondScreen({super.key});
 
-  @override
-  State<SecondScreen> createState() => _SecondScreenState();
-}
-class _SecondScreenState extends State<SecondScreen> {
+class LoginPage extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  Future<User?> _handleSignIn() async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+      await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount!.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+      final UserCredential authResult =
+      await _auth.signInWithCredential(credential);
+      final User? user = authResult.user;
+      return user;
+    } catch (error) {
+      print("Error during Google sign-in: $error");
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Column(
-            children:[
-              SvgPicture.asset("assets/images/Logo11.svg",semanticsLabel: 'Acme Logo',),
-              SizedBox(height: 20,),
-              Row(
+        appBar: AppBar(
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: SvgPicture.asset("assets/images/Logo11.svg",semanticsLabel: 'Acme Logo',),
+              ),
+              SizedBox(height: 30),
+              Column(
                 children: [
-                  Text("Lets begin",
-
+                  Row(
+                    children: [
+                      Text("Let's begin",
                           style: GoogleFonts.poppins(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w700,
-                      )),
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                          )),
+                    ],
+                  ),
                 ],
-
-                // children: [
-                //   Text("Lets begin",
-                //       textAlign:TextAlign.right,
-                //       style: GoogleFonts.poppins(
-                //     fontSize: 22,
-                //     fontWeight: FontWeight.w700,
-                //   )),
-                //
-                // ],
               ),
-              Text("Login with your google account to continue journey to world of Electronics gadgets ",
-                  style: GoogleFonts.poppins(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
+              SizedBox(height: 10),
+              Text(
+                'Login with your google account to continue journey to world of electronic gadgets',
+                style: GoogleFonts.poppins(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(height: 100),
+              Container(
+                  child: SignInButton(
+                    Buttons.google,
+                    onPressed: () async {
+                      User? user = await _handleSignIn();
+                      print('dataaaaaaaaaaaaaa${user!.displayName.toString()}');
+                      if (user != null) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Thirdpage(user),
+                          ),
+                        );
+                      }
+
+                    },
                   )
-              ),
-
-              SizedBox(height: 40,),
-              ElevatedButton(onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Thirdpage()),
-                );
-              }, child: Text("Sign in with google")),
-
-
+              )
             ],
           ),
-        ),
+        )
     );
   }
 }
+
+// class UserDetailsPage extends StatelessWidget {
+//   final User user;
+//
+//   UserDetailsPage(this.user);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("User Details"),
+//         actions: [],
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text("Welcome, ${user.displayName}"),
+//             Text("Email: ${user.email}"),
+//             Text("UID: ${user.uid}"),
+//             IconButton(
+//               icon: Icon(Icons.logout),
+//               onPressed: () async {
+//                 await FirebaseAuth.instance.signOut();
+//                 await GoogleSignIn().signOut();
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => Thirdpage()),
+//                 );
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+// import 'dart:async';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:sign_in_button/sign_in_button.dart';
+// import 'Thirdpage.dart';
+//
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: FirebaseOptions(
+//       apiKey: "AIzaSyBr12E7ktX8NwWlKb0P11qlhAfI46TC22c",
+//       appId: "1:1026123339623:android:b757cd47f785d1594881c2",
+//       messagingSenderId: "1026123339623",
+//       projectId: "pro3-f9139",
+//     ),
+//   );
+//   runApp(const MyApp());
+//
+//
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, });
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   void initState() {
+//     super.initState();
+//     Timer(Duration(seconds: 3),
+//             ()=>Navigator.pushReplacement(context,
+//             MaterialPageRoute(builder:
+//                 (context) =>
+//                 SecondScreen()
+//             )
+//         )
+//     );
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Container(
+//           height: 104,
+//           width: 104,
+//           child: SvgPicture.asset("assets/images/Logo112.svg"),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// class SecondScreen extends StatefulWidget {
+//   const SecondScreen({super.key});
+//
+//   @override
+//   State<SecondScreen> createState() => _SecondScreenState();
+// }
+// class _SecondScreenState extends State<SecondScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body:Column(
+//           children:[
+//             Container(
+//               color: Color(0xff4b0098),
+//             ),
+//             SvgPicture.asset("assets/images/Logo11.svg",semanticsLabel: 'Acme Logo',),
+//             SizedBox(height: 20,),
+//             Row(
+//               children: [
+//                 Text("Lets begin",
+//                         style: GoogleFonts.poppins(
+//                       fontSize: 22,
+//                       fontWeight: FontWeight.w700,
+//                     )),
+//               ],
+//             ),
+//             Text("Login with your google account to continue journey to world of Electronics gadgets ",
+//                 style: GoogleFonts.poppins(
+//                   fontSize: 14,
+//                   fontWeight: FontWeight.w500,
+//                 )
+//             ),
+//             SizedBox(height: 40,),
+//             SignInButton(
+//               Buttons.google,
+//               text: "Sign up with Google",
+//               onPressed: () {},
+//             )
+//           ],
+//         ),
+//     );
+//   }
+// }
+// class LoginPage extends StatelessWidget {
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+//   final GoogleSignIn googleSignIn = GoogleSignIn();
+//
+//   Future<User?> _handleSignIn() async {
+//     try {
+//       final GoogleSignInAccount? googleSignInAccount =
+//       await googleSignIn.signIn();
+//       final GoogleSignInAuthentication googleSignInAuthentication =
+//       await googleSignInAccount!.authentication;
+//       final AuthCredential credential = GoogleAuthProvider.credential(
+//         accessToken: googleSignInAuthentication.accessToken,
+//         idToken: googleSignInAuthentication.idToken,
+//       );
+//       final UserCredential authResult =
+//       await _auth.signInWithCredential(credential);
+//       final User? user = authResult.user;
+//       return user;
+//     } catch (error) {
+//       print("Error during Google sign-in: $error");
+//       return null;
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.blue,
+//         title: Text("Login with Google"),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () async {
+//             User? user = await _handleSignIn();
+//             print('dataaaaaaaaaaaaaa${user!.displayName.toString()}');
+//             if (user != null) {
+//               Navigator.pushReplacement(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: (context) => UserDetailsPage(user),
+//                 ),
+//               );
+//             }
+//           },
+//           child: Text("Sign In with Google"),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// class UserDetailsPage extends StatelessWidget {
+//   final User user;
+//
+//   UserDetailsPage(this.user);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("User Details"),
+//         actions: [],
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text("Welcome, ${user.displayName}"),
+//             Text("Email: ${user.email}"),
+//             Text("UID: ${user.uid}"),
+//             IconButton(
+//               icon: Icon(Icons.logout),
+//               onPressed: () async {
+//                 await FirebaseAuth.instance.signOut();
+//                 await GoogleSignIn().signOut();
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => Thirdpage()),
+//                 );
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }}
